8ad178eae82711f6070a7620877e8278ae61b5d4
Eriri <741674999@qq.com>
2019-04-02 13:29:05 +0800

    42

--- server.py
+++ server.py
@@ -14,9 +14,13 @@ proj_path, proj_ss, proj_db = None, None, None
 
 class MainFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, parent=None, title="filetrans",
-                          size=(640, 400), style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        wx.Frame.__init__(self, parent=None, title="filetrans", size=(570, 380),
+                          style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
         self.Bind(wx.EVT_CLOSE, self.destroy)
+
+        PMF = ProblemFrame(parent=self)
+        WMF = WorkFrame(parent=self)
+        PMF.Show(False), WMF.Show(False)
 
         MB, ME, ELSE = wx.MenuBar(), wx.Menu(), wx.Menu()
         MB.Append(ME, "工作区"), MB.Append(ELSE, "其他")
@@ -28,31 +32,33 @@ class MainFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.destroy, exit_)
         about_ = ELSE.Append(-1, "关于")
         self.Bind(wx.EVT_MENU, self.about, about_)
-
         self.SetMenuBar(MB)
-        self.Bs = wx.Button(parent=self, label="学生管理",
-                            size=(120, 60), pos=(460, 20))
-        self.Bp = wx.Button(parent=self, label="题目管理",
-                            size=(120, 60), pos=(460, 100))
-        self.Bw = wx.Button(parent=self, label="作业管理",
-                            size=(120, 60), pos=(460, 180))
-        self.Bt = wx.Button(parent=self, label="系统评测",
-                            size=(120, 60), pos=(460, 260))
-        self.Buttons = [self.Bs, self.Bp, self.Bw, self.Bt]
+
+        Bs = wx.Button(parent=self, label="学生管理", size=(120, 60), pos=(420, 10))
+        self.Bind(wx.EVT_BUTTON,lambda e:self.add_mf("S"), Bs)
+
+        Bp = wx.Button(parent=self, label="题目管理", size=(120, 60), pos=(420, 90))
+        self.Bind(wx.EVT_BUTTON, lambda e: (PMF.Show(True), self.Freeze()), Bp)
+
+        Bw = wx.Button(parent=self, label="作业管理", size=(120, 60), pos=(420, 170))
+        self.Bind(wx.EVT_BUTTON, lambda e: (WMF.Show(True), self.Freeze()), Bw)
+
+        Bt = wx.Button(parent=self, label="系统评测", size=(120, 60), pos=(420, 250))
+
+        self.Buttons = [Bs, Bp, Bw, Bt]
         for b in self.Buttons:
             b.Show(True), b.Disable()
 
-        self.OLV = ObjectListView(parent=self, pos=(20, 20), size=(400, 300),
+        self.OLV = ObjectListView(parent=self, pos=(10, 10), size=(400, 300),
                                   sortable=True, style=wx.LC_REPORT | wx.BORDER_SUNKEN)
         self.OLV.SetColumns([
-            ColumnDefn("学号", "left", 120, "Id"),
-            ColumnDefn("姓名", "left", 120, "name"),
-            ColumnDefn("登陆时间", "left", 120, "ctime"),
-            ColumnDefn("提交情况", "left", 120, "ws"),
-            ColumnDefn("登录密码", "left", 120, "pwd")
+            ColumnDefn("学号", "left", 100, "Id"),
+            ColumnDefn("姓名", "left", 80, "name"),
+            ColumnDefn("登陆时间", "left", 100, "ctime"),
+            ColumnDefn("提交情况", "left", 100, "ws"),
+            ColumnDefn("登录密码", "left", 100, "pwd")
         ])
         self.OLV.SetEmptyListMsg("")
-        self.OLV.Show(True)
 
     def destroy(self, event):
         global proj_path, proj_ss
@@ -61,8 +67,7 @@ class MainFrame(wx.Frame):
         self.Destroy()
 
     def open_proj(self, event=None):
-        dd = wx.DirDialog(parent=self, message="选择工作区文件夹",
-                          defaultPath=sys.path[0])
+        dd = wx.DirDialog(parent=self, message="选择工作区文件夹", defaultPath=sys.path[0])
         if dd.ShowModal() == wx.ID_OK:
             global proj_path, proj_ss, proj_db
             if proj_path != None:
@@ -78,8 +83,8 @@ class MainFrame(wx.Frame):
         global proj_path, proj_ss
         if proj_path != None:
             proj_path = None
-            proj_ss.server_close()
             proj_ss.shutdown()
+            proj_ss.server_close()
             for b in self.Buttons:
                 b.Disable()
             self.OLV.DeleteAllItems()
@@ -90,17 +95,92 @@ class MainFrame(wx.Frame):
     def update_olv(self, event=None):
         con, cur = open_db(proj_db)
         probs = [x[0] for x in select_all(cur, "problem_info", ["id"])]
-        items = gen_models(probs, select_all(cur, "user_info", ["*"]))
+        items = select_all(cur, "user_info", ["*"])
         close_db(con, cur)
+        models = []
+        for item in items:
+            models.append(MainModel(item[0], item[1], item[2], item[3], probs))
         self.OLV.DeleteAllItems()
-        self.OLV.AddObjects(items)
+        self.OLV.AddObjects(models)
+
+    def add_mf(self,T):
+        if T == "S":
+            MF = StudentFrame(parent=self)
+        if T == "P":
+            MF = ProblemFrame(parent=self)
+        if T == "W":
+            MF = WorkFrame(parent=self)
+        MF.Show(True)
+        self.Freeze()
 
 
-def gen_models(probs, items):
-    models = []
-    for item in items:
-        models.append(MainModel(item[0], item[1], item[2], item[3], probs))
-    return models
+
+class StudentFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent=parent, title="学生管理", size=(
+            530, 260), style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        self.Bind(wx.EVT_CLOSE, self.destroy)
+        self.OLV = ObjectListView(parent=self, pos=(10, 10), size=(380, 200),
+                                  sortable=True, style=wx.LC_REPORT | wx.BORDER_SUNKEN)
+
+        self.OLV.SetColumns([
+            ColumnDefn("学号", "left", 100, "Id"),
+            ColumnDefn("姓名", "left", 80, "name"),
+            ColumnDefn("登录密码", "left", 100, "pwd")
+        ])
+        self.OLV.CreateCheckStateColumn()
+        self.update_olv()
+
+        Ba = wx.Button(parent=self, label="添加学生", size=(100, 20), pos=(400, 10))
+
+        Bx = wx.Button(parent=self, label="从Excel添加学生", size=(100, 20), pos=(400, 40))
+
+        Bd = wx.Button(parent=self, label="删除学生", size=(100, 20), pos=(400, 70))
+
+        Bo = wx.Button(parent=self, label="导出学生到Excel", size=(100, 20), pos=(400, 100))
+
+
+
+    def destroy(self, event):
+        self.GetParent().Thaw()
+        self.GetParent().update_olv()
+        self.Destroy()
+
+    def update_olv(self, event=None):
+        con, cur = open_db(proj_db)
+        probs = [x[0] for x in select_all(cur, "problem_info", ["id"])]
+        items = select_all(cur, "user_info", ["*"])
+        close_db(con, cur)
+        models = []
+        for item in items:
+            models.append(MainModel(item[0], item[1], item[2], item[3], probs))
+        self.OLV.DeleteAllItems()
+        self.OLV.AddObjects(models)
+
+
+
+class ProblemFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent=parent, title="学生管理", size=(640, 400),
+                          style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        self.Bind(wx.EVT_CLOSE, self.destroy)
+
+    def destroy(self, event):
+        self.Show(False)
+        self.GetParent().Thaw()
+        self.GetParent().update_olv()
+
+
+class WorkFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent=parent, title="学生管理", size=(
+            640, 400), style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        self.Bind(wx.EVT_CLOSE, self.destroy)
+
+    def destroy(self, event):
+        self.Show(False)
+        self.GetParent().Thaw()
+        self.GetParent().update_olv()
 
 
 class MainModel(object):
@@ -109,15 +189,14 @@ class MainModel(object):
         self.name = name
         self.pwd = pwd
         self.ctime = ctime
-        for i in range(len(probs)):
-            if os.path.isfile(os.path.join(proj_path, Id, probs[i])) == False:
-                probs[i] = "□"*len(probs[i])
-        self.ws = "|".join(probs)
-
-
-class StudentFrame(wx.Frame):
-    def __init__(self, parent):
-        wx.Frame.__init__(self, parent=parent, title="学生管理")
+        vprobs = []
+        for prob in probs:
+            v = "□"*len(prob)
+            for suf in CD._LANGUAGE_CONFIG_:
+                if os.path.isfile(os.path.join(proj_path, Id, prob+suf)) == True:
+                    v = prob
+            vprobs.append(v)
+        self.ws = "|".join(vprobs)
 
 
 class MainApp(wx.App):
